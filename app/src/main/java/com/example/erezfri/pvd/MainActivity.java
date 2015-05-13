6ac82a80851e416803db59be17fd0f843504b850
idanpor <idanpor@gmail.com>
2015-05-13 12:54:18 +0300

    screens update

--- app/src/main/java/com/example/erezfri/pvd/MainActivity.java
+++ app/src/main/java/com/example/erezfri/pvd/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.erezfri.pvd;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
-
+import android.view.View;
+import android.widget.Button;
 
 
 //test
@@ -16,8 +17,16 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener (new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),NextPageAferStart.class);
+                startActivity(i);
+            }
+        });
     }
-//erezgayss
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
---
+++ app/src/main/java/com/example/erezfri/pvd/NextPageAferStart.java
@@ -0,0 +1,24 @@
+/**
+ * Created by idanpor on 27/04/2015.
+ */
+package com.example.erezfri.pvd;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+
+public class NextPageAferStart extends ActionBarActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.newpageafterstart);
+
+       // final Button button = (Button) findViewById(R.id.button);
+    }
+    }
