4168a07387649774f698c5886b9598f0b333f7c3
erezfri <erezfri@gmail.com>
2015-05-15 15:05:04 +0300

    update main screen + alert dialog message

--- app/src/main/java/com/example/erezfri/pvd/MainActivity.java
+++ app/src/main/java/com/example/erezfri/pvd/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.erezfri.pvd;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,23 +10,38 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
-
-//test
-
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
+        builder1.setTitle("Welcome to the PVD Detector app");
+        builder1.setMessage(
+                "This app will help you to determine if you are at risk for \n" +
+                        "the Peripheral Vascular Disease (PVD). For best results you " +
+                        "should perform the test at least 3 times in 3 different days.\n" +
+                        "Each test takes 15 minutes and you can stop it \n" +
+                        "in the middle while the results will not be saved. \n" +
+                        "In the end of the test we will show you a report of \n" +
+                        "the current test and how many more test you need to \n" +
+                        "perform. After you are done with the 3 tests we will \n" +
+                        "analyze the data and we will show you a conclusion.");
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener (new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent i = new Intent(getApplicationContext(),NextPageAferStart.class);
-                startActivity(i);
-            }
-        });
+        builder1.setCancelable(true);
+        builder1.setPositiveButton("Got it, let's start",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+
+
+        AlertDialog alert11 = builder1.create();
+        alert11.show();
+
+
     }
 
 
