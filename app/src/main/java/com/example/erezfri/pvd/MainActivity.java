7ca119c74087c5130349e5ffb0d500ca5567c491
erezfri <erezfri@gmail.com>
2015-05-20 20:04:02 +0300

    Adding the counter

--- app/src/main/java/com/example/erezfri/pvd/MainActivity.java
+++ app/src/main/java/com/example/erezfri/pvd/MainActivity.java
@@ -4,105 +4,113 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-import android.location.Location;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends ActionBarActivity {
-    Button btnShowLocation;
-    GPSTracker gps;
-    int counter = 0 ;
-    Location loc1 = new Location("old");
-    Location loc2 = new Location("new");
+
+    private TextView tempTextView; //Temporary TextView
+    private Button tempBtn; //Temporary Button
+    private Handler mHandler = new Handler();
+    private long startTime;
+    private long elapsedTime;
+    private final int REFRESH_RATE = 100;
+    private String hours,minutes,seconds,milliseconds;
+    private long secs,mins,hrs,msecs;
+    private boolean stopped = false;
+    private Runnable startTimer;
+
+    public MainActivity() {
+        startTimer = new Runnable() {
+            public void run() {
+                elapsedTime = System.currentTimeMillis() - startTime;
+                updateTimer(elapsedTime);
+                mHandler.postDelayed(this,REFRESH_RATE);
+            }
+        };
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AlertDialog.Builder builder1 = new AlertDialog.Builder(MainActivity.this);
-        builder1.setTitle("Welcome to the PVD Detector app");
-        builder1.setMessage(
-                "This app will help you to determine if you are at risk for \n" +
-                        "the Peripheral Vascular Disease (PVD). For best results you " +
-                        "should perform the test at least 3 times in 3 different days.\n" +
-                        "Each test takes 15 minutes and you can stop it \n" +
-                        "in the middle while the results will not be saved. \n" +
-                        "In the end of the test we will show you a report of \n" +
-                        "the current test and how many more test you need to \n" +
-                        "perform. After you are done with the 3 tests we will \n" +
-                        "analyze the data and we will show you a conclusion.");
-
-        builder1.setCancelable(true);
-        builder1.setPositiveButton("Got it, let's start",
-                new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        dialog.cancel();
-                    }
-                });
-
-
-        AlertDialog alert11 = builder1.create();
-        alert11.show();
-        final TextView tv1 = (TextView) findViewById(R.id.textView);
-
-        btnShowLocation = (Button) findViewById(R.id.button);
-        btnShowLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                gps = new GPSTracker(MainActivity.this);
-                if (gps.CanGetLocation) {
-                    counter++;
-                    if (counter % 2 == 0) {
-                        loc2.setLatitude(gps.getLatitude());
-                        loc2.setLongitude(gps.getLongitude());
-                        float distance = loc2.distanceTo(loc1);
-                        tv1.setText("(latitude1: " + loc1.getLatitude() + "," + "longitude1: " + loc1.getLongitude() + ")+" + "(latitude2: " + loc2.getLatitude() + "," + "longitude2: " + loc2.getLongitude() + ")" + "distance:" + distance + "/n");
-                        // Toast.makeText(getApplicationContext(),"("+ loc2.getLatitude()+","+loc2.getLongitude()+") " +distance,Toast.LENGTH_SHORT).show();
-                        //Toast.makeText(getApplicationContext(),"("+ (loc2.getLatitude()-gps.getLatitude())+","+(loc2.getLongitude()-gps.getLongitude())+")" ,Toast.LENGTH_LONG).show();
-
-
-                    } else {
-                        Toast.makeText(getApplicationContext(), "your location is \nlat:" + gps.getLatitude() + "\nlong:" + gps.getLongitude(), Toast.LENGTH_SHORT).show();
-                        loc1.setLatitude(gps.getLatitude());
-                        loc1.setLongitude(gps.getLongitude());
-                    }
-                }
-                else
-                {
-                    gps.showSettingAlerts();
-                }
-            }
-        });
-
-
-
     }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public void startClick(View view){
+            showStopButton();
+        if(stopped){
+            startTime = System.currentTimeMillis() - elapsedTime;
+        }
+        else{ startTime = System.currentTimeMillis();
+        }
+        mHandler.removeCallbacks(startTimer);
+        mHandler.postDelayed(startTimer, 0);
+        }
+        public void stopClick(View view){
+            hideStopButton();
+            mHandler.removeCallbacks(startTimer);
+            ((TextView)findViewById(R.id.counterText)).setText("00:00:00");
+            stopped = false;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+
+        private void showStopButton(){
+            ((Button)findViewById(R.id.startButton)).setVisibility(View.GONE);
+            ((Button)findViewById(R.id.stopButton)).setVisibility(View.VISIBLE);
+        }
+        private void hideStopButton(){
+            ((Button)findViewById(R.id.startButton)).setVisibility(View.VISIBLE);
+            ((Button)findViewById(R.id.stopButton)).setVisibility(View.GONE);
+        }
+
+        private void updateTimer (float time){
+            secs = (long)(time/1000);
+            mins = (long)((time/1000)/60);
+            hrs = (long)(((time/1000)/60)/60);
+            /* Convert the seconds to String * and format to ensure it has * a leading zero when required */
+            secs = secs % 60;
+            seconds=String.valueOf(secs);
+            if(secs == 0) {
+                seconds = "00";
+            }
+            if(secs <10 && secs > 0){
+                seconds = "0"+seconds;
+            }
+            /* Convert the minutes to String and format the String */
+            mins = mins % 60;
+            minutes=String.valueOf(mins);
+            if(mins == 0){
+                minutes = "00";
+            }
+            if(mins <10 && mins > 0){
+                minutes = "0"+minutes;
+            }
+            /* Convert the hours to String and format the String */
+            hours=String.valueOf(hrs);
+            if(hrs == 0){
+                hours = "00";
+            }
+            if(hrs <10 && hrs > 0){
+                hours = "0"+hours;
+            }
+            /* Although we are not using milliseconds on the timer in this example * I included the code in the event that you wanted to include it on your own */
+            milliseconds = String.valueOf((long)time);
+            if(milliseconds.length()==2){
+                milliseconds = "0"+milliseconds;
+            }
+            if(milliseconds.length()<=1){
+                milliseconds = "00";
+            }
+            milliseconds = milliseconds.substring(milliseconds.length()-3, milliseconds.length()-2);
+             /* Setting the timer text to the elapsed time */
+            ((TextView)findViewById(R.id.counterText)).setText(hours + ":" + minutes + ":" + seconds);
+          //  ((TextView)findViewById(R.id.timerMs)).setText("." + milliseconds);
+        }
+
 }
+
