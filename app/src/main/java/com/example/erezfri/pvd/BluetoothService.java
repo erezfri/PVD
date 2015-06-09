31a10334167401f92734e282ee0c6ec9db15fd4a
erezfri <erezfri@gmail.com>
2015-06-09 15:11:29 +0300

    Bluetooth service and plot dynamic

---
+++ app/src/main/java/com/example/erezfri/pvd/BluetoothService.java
@@ -0,0 +1,485 @@
+package com.example.erezfri.pvd;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+
+/**
+ * This class does all the work for setting up and managing Bluetooth
+ * connections with other devices. It has a thread that listens for
+ * incoming connections, a thread for connecting with a device, and a
+ * thread for performing data transmissions when connected.
+ */
+public class BluetoothService {
+	 // Debugging
+    private static final String TAG = "BluetoothChatService";
+    private static final boolean D = true;
+
+    // Name for the SDP record when creating server socket
+    private static final String SERVICE_NAME = "BluetoothSecure";
+
+    // Unique UUID for this application
+    private static final UUID MY_UUID =
+        UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
+
+
+    // Member fields
+    private final BluetoothAdapter mAdapter;
+    private final Handler mHandler;
+    private AcceptThread mAcceptThread;
+    private ConnectThread mConnectThread;
+    private ConnectedThread mConnectedThread;
+    private int mState;
+    private int mConnectSide; 
+
+    public static int mBufLength = 1024;
+    
+    // Constants that indicate connection side mechanism used in device to establish
+    public static final int SERVER = 0;
+    public static final int CLIENT = 1;
+    public static final int BOTH = 2;
+    
+    // Constants that indicate the current connection state
+    public static final int STATE_NONE = 0;       // we're doing nothing
+    public static final int STATE_LISTEN = 1;     // now listening for incoming connections
+    public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
+    public static final int STATE_CONNECTED = 3;  // now connected to a remote device
+    
+    // Message types sent from the BluetoothService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    
+    // Key names send from the BluetoothService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+    
+    public static boolean READ_BUF_ALLOCATE=false; // determine if each read a new buffer will be allocated (true - only if you want to stuck the buffers, if not the first buffer will overide)
+        
+    
+    /**
+     * Constructor. Prepares a new BluetoothChat session.
+     * @param context  The UI Activity Context
+     * @param handler  A Handler to send messages back to the UI Activity
+     * @param connectSide connect as: client or server for one implementation each for one explicit device, 
+     *                                or both for other implementation, where prepare each device as a server.  
+     */
+    public BluetoothService(Context context, Handler handler,int connectSide) {
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+        mState = STATE_NONE;
+        mHandler = handler;
+        mConnectSide = connectSide;      
+        if(mConnectSide == CLIENT) setState(mState);
+        
+    }
+
+    /**
+     * Set the current state of the connection
+     * @param state  An integer defining the current connection state
+     */
+    private synchronized void setState(int state) {
+        if (D) Log.d(TAG, "setState() " + mState + " -> " + state);
+        mState = state;
+
+        // Give the new state to the Handler so the UI Activity can update
+        mHandler.obtainMessage(MESSAGE_STATE_CHANGE, state, -1).sendToTarget();
+    }
+
+    /**
+     * Return the current connection state. */
+    public synchronized int getState() {
+        return mState;
+    }
+
+    /**
+     * Set Bluetooth service. Start AcceptThread to begin a session in listening (server) mode if needed. 
+     * Called by the Activity onResume() */
+    public synchronized void set() {
+        if (D) Log.d(TAG, "start");
+
+        // Cancel any thread attempting to make a connection
+        if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
+
+        // Cancel any thread currently running a connection
+        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
+        
+        if (mConnectSide == SERVER || mConnectSide == BOTH){
+        	
+        	setState(STATE_LISTEN);
+        	// Start the thread to listen on a BluetoothServerSocket
+        	if (mAcceptThread == null) {
+        		mAcceptThread = new AcceptThread(true);
+        		mAcceptThread.start();
+        	}	
+        }
+        else{
+        	setState(STATE_NONE);
+        }
+       
+    }
+
+    /**
+     * Start the ConnectThread to initiate a connection to a remote device.
+     * @param device  The BluetoothDevice to connect
+     */
+    public synchronized void connect(BluetoothDevice device) {
+        if (D) Log.d(TAG, "connect to: " + device);
+
+        // Cancel any thread attempting to make a connection
+        if (mState == STATE_CONNECTING) {
+            if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
+        }
+
+        // Cancel any thread currently running a connection
+        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
+
+        // Start the thread to connect with the given device
+        mConnectThread = new ConnectThread(device);
+        mConnectThread.start();
+        setState(STATE_CONNECTING);
+    }
+
+    /**
+     * Start the ConnectedThread to begin managing a Bluetooth connection
+     * @param socket  The BluetoothSocket on which the connection was made
+     * @param device  The BluetoothDevice that has been connected
+     */
+    public synchronized void connected(BluetoothSocket socket, BluetoothDevice device) {
+        if (D) Log.d(TAG, "connected");
+
+        // Cancel the thread that completed the connection
+        if (mConnectThread != null) {mConnectThread.cancel(); mConnectThread = null;}
+
+        // Cancel any thread currently running a connection
+        if (mConnectedThread != null) {mConnectedThread.cancel(); mConnectedThread = null;}
+
+        // Cancel the accept thread because we only want to connect to one device
+        if (mAcceptThread != null) {
+        	mAcceptThread.cancel();
+        	mAcceptThread = null;
+        }
+
+        // Start the thread to manage the connection and perform transmissions
+        mConnectedThread = new ConnectedThread(socket);
+        mConnectedThread.start();
+
+        // Send the name of the connected device back to the UI Activity
+        Message msg = mHandler.obtainMessage(MESSAGE_DEVICE_NAME);
+        Bundle bundle = new Bundle();
+        bundle.putString(DEVICE_NAME, device.getName());
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+
+        setState(STATE_CONNECTED);
+    }
+
+    /**
+     * Stop all threads
+     */
+    public synchronized void stop() {
+        if (D) Log.d(TAG, "stop");
+
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        if (mAcceptThread != null) {
+        	mAcceptThread.cancel();
+        	mAcceptThread = null;
+        }
+        setState(STATE_NONE);
+    }
+
+    /**
+     * Write to the ConnectedThread in an unsynchronized manner
+     * @param out The bytes to write
+     * @see ConnectedThread#write(byte[])
+     */
+    public void write(byte[] out) {
+        // Create temporary object
+        ConnectedThread r;
+        // Synchronize a copy of the ConnectedThread
+        synchronized (this) {
+            if (mState != STATE_CONNECTED) return;
+            r = mConnectedThread;
+        }
+        // Perform the write unsynchronized
+        r.write(out);
+    }
+
+    /**
+     * Indicate that the connection attempt failed and notify the UI Activity.
+     */
+    private void connectionFailed() {
+        // Send a failure message back to the Activity
+        Message msg = mHandler.obtainMessage(MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(TOAST, "Unable to connect device");
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+
+        // Start the service over to restart listening mode
+        BluetoothService.this.set();
+    }
+
+    /**
+     * Indicate that the connection was lost and notify the UI Activity.
+     */
+    private void connectionLost() {
+        // Send a failure message back to the Activity
+        Message msg = mHandler.obtainMessage(MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(TOAST, "Device connection was lost");
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+
+        // Start the service over to restart listening mode
+        BluetoothService.this.set();
+    }
+
+    /**
+     * This thread runs while listening for incoming connections. It behaves
+     * like a server-side client. It runs until a connection is accepted
+     * (or until cancelled).
+     */
+    private class AcceptThread extends Thread {
+        // The local server socket
+        private final BluetoothServerSocket mmServerSocket;
+
+        public AcceptThread(boolean secure) {
+            BluetoothServerSocket tmp = null;
+
+            // Create a new listening server socket
+            try {
+            	tmp = mAdapter.listenUsingRfcommWithServiceRecord(SERVICE_NAME, MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG,"listen() failed", e);
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            if (D) Log.d(TAG,"BEGIN mAcceptThread" + this);
+            setName("AcceptThread");
+
+            BluetoothSocket socket = null;
+
+            // Listen to the server socket if we're not connected
+            while (mState != STATE_CONNECTED) {
+                try {
+                    // This is a blocking call and will only return on a
+                    // successful connection or an exception
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    Log.e(TAG, "accept() failed", e);
+                    break;
+                }
+
+                // If a connection was accepted
+                if (socket != null) {
+                    synchronized (BluetoothService.this) {
+                        switch (mState) {
+                        case STATE_LISTEN:
+                        case STATE_CONNECTING:
+                            // Situation normal. Start the connected thread.
+                            connected(socket, socket.getRemoteDevice());
+                            break;
+                        case STATE_NONE:
+                        case STATE_CONNECTED:
+                            // Either not ready or already connected. Terminate new socket.
+                            try {
+                                socket.close();
+                            } catch (IOException e) {
+                                Log.e(TAG, "Could not close unwanted socket", e);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+            if (D) Log.i(TAG, "END mAcceptThread");
+
+        }
+
+        public void cancel() {
+            if (D) Log.d(TAG, "cancel " + this);
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "close() of server failed", e);
+            }
+        }
+    }
+
+
+    /**
+     * This thread runs while attempting to make an outgoing connection
+     * with a device. It runs straight through; the connection either
+     * succeeds or fails.
+     */
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+
+        public ConnectThread(BluetoothDevice device) {
+            mmDevice = device;
+            BluetoothSocket tmp = null;
+
+            // Get a BluetoothSocket for a connection with the
+            // given BluetoothDevice
+            try {
+            	tmp = device.createRfcommSocketToServiceRecord(
+                        MY_UUID);
+            } catch (IOException e) {
+                Log.e(TAG, "create() failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            Log.i(TAG, "BEGIN mConnectThread");
+            setName("ConnectThread");
+
+            // Always cancel discovery because it will slow down a connection
+            mAdapter.cancelDiscovery();
+
+            // Make a connection to the BluetoothSocket
+            try {
+                // This is a blocking call and will only return on a
+                // successful connection or an exception
+                mmSocket.connect();
+            } catch (IOException e) {
+                // Close the socket
+                try {
+                    mmSocket.close();
+                } catch (IOException e2) {
+                    Log.e(TAG, "unable to close() socket during connection failure", e2);
+                }
+                connectionFailed();
+                return;
+            }
+
+            // Reset the ConnectThread because we're done
+            synchronized (BluetoothService.this) {
+                mConnectThread = null;
+            }
+
+            // Start the connected thread
+            connected(mmSocket, mmDevice);
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "close() of connect, socket failed", e);
+            }
+        }
+    }
+
+    /**
+     * This thread runs during a connection with a remote device.
+     * It handles all incoming and outgoing transmissions.
+     */
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            Log.d(TAG, "create ConnectedThread");
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the BluetoothSocket input and output streams
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "temp sockets not created", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            Log.i(TAG, "BEGIN mConnectedThread");
+            byte[] buffer = new byte[mBufLength];
+            int bytes;
+           
+            // Keep listening to the InputStream while connected
+            while (true) {
+                try {
+                	if (READ_BUF_ALLOCATE) 
+                		buffer = new byte[mBufLength]; //allocate new buffer if the buffers are stuck
+                	
+                	//buffer = new byte[mBufLength];
+                    // Read from the InputStream
+                    bytes = mmInStream.read(buffer);
+
+                    // Send the obtained bytes to the UI Activity
+                    mHandler.obtainMessage(MESSAGE_READ, bytes, -1, buffer)
+                            .sendToTarget();
+                } catch (IOException e) {
+                    Log.e(TAG, "disconnected", e);
+                    connectionLost();
+                    
+                    // Start the service over to restart listening mode
+                    //BluetoothService.this.set();
+                    break;   
+                }
+            }
+        }
+
+        /**
+         * Write to the connected OutStream.
+         * @param buffer  The bytes to write
+         */
+        public void write(byte[] buffer) {
+            try {
+                mmOutStream.write(buffer);
+
+                // Share the sent message back to the UI Activity
+                mHandler.obtainMessage(MESSAGE_WRITE, -1, -1, buffer)
+                        .sendToTarget();
+            } catch (IOException e) {
+                Log.e(TAG, "Exception during write", e);
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "close() of connect socket failed", e);
+            }
+        }
+    }
+    
+    
+    
+
+}
---
+++ app/src/main/java/com/example/erezfri/pvd/PlotDynamic.java
@@ -0,0 +1,358 @@
+package com.example.erezfri.pvd;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.Align;
+import android.view.View;
+
+/**
+ * @author Eliyahu Sason
+ * This class plot some Dynamic graphs in a scorll mode like a scope.
+ * - fixed y range [mMaxy,mMaxy] is assumed.
+ * - when u add data u need to specify the graph numer
+ * - u can define horizontal axes length, change the graph colors, add grid and legend.   
+ */
+
+public class PlotDynamic extends View{
+	private Paint mPaintAxes,mPaintText;
+	private ArrayList<Paint> mPaint;
+	private int mPlotNum;
+	private String mTitle=null;
+	private String[] mPlotName=null;
+	private boolean mGrid=false;
+	private int mylinesNum=5;
+	
+	private int[] mColorConstants={Color.RED,Color.GREEN,Color.CYAN,Color.MAGENTA};
+	private int mColorGrid;
+	private int mColorBackgroud;
+	public enum Colorpart{plots,grid,backgroud};
+	
+	private ArrayList<ArrayList<Integer>> yvaluesGroup;
+	private ArrayList<ArrayList<Float>> xvaluesGroup;
+		
+	private float mMaxy=5,mMiny=-5,mPixels;
+	private float mxInterval =10;
+	private Canvas mCanvas;
+	private float mcanvasHeight,mcanvasWidth;
+	private int initiate = 0;
+	
+	
+	/**
+	 * PlotDynamic's Constructor - Creates and initializes PlotDynamic object
+	 * @param context 
+	 * @param plotNum - number of plots - must be natural number
+	 * @param xInterval - the horizontal axes length - must be real and positive number 
+	 */
+	public PlotDynamic(Context context,int plotNum, float xInterval) {
+		super(context);
+		mPlotNum=plotNum;
+		yvaluesGroup=new ArrayList<ArrayList<Integer>>(mPlotNum);
+		xvaluesGroup=new ArrayList<ArrayList<Float>>(mPlotNum);
+		mPaint =new ArrayList<Paint>(mPlotNum);
+				
+		for (int i=0;i<mPlotNum;i++){
+			yvaluesGroup.add(new ArrayList<Integer>());	
+			xvaluesGroup.add(new ArrayList<Float>());
+			
+			Paint paint = new Paint();
+			paint.setStrokeWidth(1);
+			paint.setColor(mColorConstants[i]);
+			paint.setTextSize(20.0f);
+			
+			mPaint.add(paint);		
+		}
+		
+		mPaintAxes=new Paint();
+		mPaintAxes.setStrokeWidth(2);
+		
+		mPaintText=new Paint();
+		mPaintText.setColor(Color.BLACK);
+		
+		mxInterval=xInterval;
+	}
+
+	/**
+	 * Called when the view should render its content. 
+	 */
+	@Override
+	protected void onDraw(Canvas canvas) {
+		mCanvas=canvas;
+		
+		if (initiate == 0){
+			mcanvasHeight = canvas.getHeight();
+			mcanvasWidth = canvas.getWidth();
+			mPixels=mcanvasHeight;
+			initiate = 1;
+		}
+		   
+		canvas.drawColor(mColorBackgroud);
+		
+		setAxes();
+		setGrid();
+		Legend();
+		setTitle();
+		
+	    int[][] xvaluesInPixels = new int[mPlotNum][];
+	    for (int i=0;i<mPlotNum;i++){
+	    	xvaluesInPixels[i]=toPixel(mcanvasWidth,xvaluesGroup.get(i));
+	    }
+	    
+		for (int i=0;i<mPlotNum;i++){	
+			ArrayList<Integer> yvalues = yvaluesGroup.get(i);
+		if (yvalues.size()>1){
+			int[] xvalues = xvaluesInPixels[i];
+			Paint paint = mPaint.get(i);
+			
+			for (int j = 0; j < yvalues.size()-1; j++) {
+				//canvas.drawCircle(xvalues[j],mcanvasHeight-yvalues.get(j).intValue(),2,paint);
+				//canvas.drawCircle(xvalues[j+1],mcanvasHeight-yvalues.get(j+1).intValue(),2,paint);
+				canvas.drawLine(xvalues[j],mcanvasHeight-yvalues.get(j).intValue(),xvalues[j+1],mcanvasHeight-yvalues.get(j+1).intValue(),paint);
+			}
+		}
+			
+		}	
+	}
+	
+	/**
+	 * AddData add one point to one of the plots.
+	 * u need to specify x and y values and a valid plot index.
+	 */
+	public void addData(float x,float y,int plotInd){
+		ArrayList<Integer> yvalues = yvaluesGroup.get(plotInd);  
+		ArrayList<Float> xvalues = xvaluesGroup.get(plotInd);  
+		
+		// add y value as pixel
+		yvalues.add(Integer.valueOf(toPixel(y)));
+		xvalues.add(Float.valueOf(x));
+		
+		// add x value as value
+		while(true){
+			float Minx = xvalues.get(0);
+			if (x>Minx+mxInterval){
+				yvalues.remove(0);
+				xvalues.remove(0);
+			}
+			else
+				break;
+		}
+	}
+	
+	/*
+	public void clearData(){
+		Float temp = xvalues.get(0);
+		xvalues.clear();
+		xvalues.add(temp);
+		
+		temp = yvalues.get(0);
+		yvalues.clear();
+		yvalues.add(temp);
+	}*/
+	
+	/*
+	private float[] getFloat(ArrayList<Float> value) {
+		
+		float[] v = new float[value.size()];
+		
+		int i=0;
+		for (Float f:value){
+			v[i++]=(f != null ? f.floatValue() : Float.NaN); 
+		}
+		
+		return (v);
+	}*/
+	
+	
+	
+	/**
+	 * Set a legend to the graph
+	 * u need to specify an array of plot names with length equals to the plot number
+	 */
+	public void Legend(String[] plotName){
+		mPlotName=plotName;
+	}
+	private void Legend(){
+		if (mPlotName!=null){
+			double[] posTitles = new double[]{0.75*mcanvasWidth,0.13*mcanvasHeight};
+			double[] posTitlesSign = new double[]{(0.75-0.03)*mcanvasWidth,(0.13-0.005)*mcanvasHeight};
+		
+			double deltaTitle=0.03*mcanvasHeight;
+			double[] deltaTitleSign=new double[]{0.015*mcanvasWidth,deltaTitle};
+			mPaintText.setTextSize(15.0f);
+			mPaintText.setTextAlign(Align.LEFT);
+		
+			for(int i=0;i<mPlotNum;i++){
+				mCanvas.drawText(mPlotName[i], (float)posTitles[0],(float)(i*deltaTitle+posTitles[1]), mPaintText);
+				//mCanvas.drawCircle((float)posTitlesSign[0], (float)(posTitlesSign[1]+i*deltaTitleSign[1]), 3, mPaint.get(i));
+				mCanvas.drawLine((float)(posTitlesSign[0]-deltaTitleSign[0]), (float)(posTitlesSign[1]+i*deltaTitleSign[1]), (float)(posTitlesSign[0]+deltaTitleSign[0]), (float)(posTitlesSign[1]+i*deltaTitleSign[1]),  mPaint.get(i));	
+				}
+			float delta = mcanvasHeight/(mylinesNum+1);
+			float deltaTime=delta/mcanvasWidth*mxInterval;
+			mCanvas.drawText(("Time segment is: "+String.format("%.2f", deltaTime)), (float)posTitles[0],(float)(mPlotNum*deltaTitle+posTitles[1]), mPaintText);
+			
+			}
+		
+		
+		}
+	
+	/**
+	 * Sets the main title of the graph
+	 * u need to specify the title.
+	 */
+	public void setTitle(String title){
+		mTitle=title;
+	}
+	private void setTitle(){
+		if(mTitle!=null){
+			mPaintText.setTextSize(20.0f);
+			mPaintText.setTextAlign(Align.CENTER);
+			mCanvas.drawText(mTitle, (float)0.5*mcanvasWidth,(float)0.1*mcanvasHeight, mPaintText);
+		}
+	}
+	
+	/**
+	 * Sets the grid on or off accordingly to the GridOn variable
+	 * true mean on, false mean off.
+	 */
+	public void setGrid(boolean GridOn){
+		mGrid=GridOn;
+	}
+	private void setGrid(){
+		if(mGrid){
+			mylinesNum=5;//odd
+			float delta = mcanvasHeight/(mylinesNum+1);
+			int xlinesNum=(int)((0.9*mcanvasWidth)/delta);
+			mPaintAxes.setColor(mColorGrid);
+			//plot y grid lines except the x axes
+			for (int i=1;i<=mylinesNum;i++){
+				if(i==mylinesNum/2+1) continue; // the line in the middle is the 0 line
+				mCanvas.drawLine((float)0.1*mcanvasWidth,i*delta,mcanvasWidth,i*delta,mPaintAxes);		
+			}
+			//plot x grid lines
+			for(int i=1;i<=xlinesNum;i++){
+				mCanvas.drawLine((float)(0.1*mcanvasWidth+i*delta),0,(float)(0.1*mcanvasWidth+i*delta),mcanvasHeight,mPaintAxes);
+			}
+		}
+	}
+	
+	
+	/**
+	 * Sets the Axes of the graph and put values besides y-axes's grid lines 
+	 */
+	private void setAxes(){
+		//plot Axes
+		mPaintAxes.setColor(Color.BLACK);
+		mCanvas.drawLine(0,mcanvasHeight/2,mcanvasWidth,mcanvasHeight/2,mPaintAxes);
+		mCanvas.drawLine((float)0.1*mcanvasWidth,0,(float)0.1*mcanvasWidth,mcanvasHeight,mPaintAxes);
+		
+		//Put values
+		mPaintText.setTextSize(20.0f);
+		mPaintText.setTextAlign(Align.RIGHT);
+		float deltaVal = (mMaxy-mMiny)/(mylinesNum+1);//delta in values
+		float deltaPixel = mcanvasHeight/(mylinesNum+1);
+		
+		for (int i=1;i<=mylinesNum;i++){
+			//if(i==mylinesNum/2+1) continue;
+			mCanvas.drawText(String.format("%.2f", (mMaxy-deltaVal*i) ), (float)(0.1-0.015)*mcanvasWidth, (float)(i*deltaPixel+0.01*mcanvasHeight), mPaintText);	
+		}
+	}
+	
+	/**
+	 * Set the plots' colors to the different parts of the graph. 
+	 * @param colorArray - an array of colors with length determined according 
+	 *        to the object u fix it's color  
+	 * @param which - the part u fix it's color
+	 */
+	public void setColor(String[] colorArray,Colorpart which){
+		switch (which){
+		case plots:
+			for (int i=0;i<mPlotNum;i++){
+				String colors= colorArray[i];
+				int color=Color.parseColor(colors);
+				mPaint.get(i).setColor(color);	
+		}
+			break;
+		case grid:
+			mColorGrid = Color.parseColor(colorArray[0]);
+			
+			break;
+		case backgroud:
+			mColorBackgroud = Color.parseColor(colorArray[0]);
+			break;
+		}
+	}
+	
+	/**
+	 * Inner function. Converts from one value to pixel.used for the y axes.  
+	 */
+	private int toPixel(float value){
+		double p;
+		float halfpixels=mPixels/2;
+		
+		if(value>0){
+			//p = halfpixels+(value/mMaxy)*.9*halfpixels;
+			p = halfpixels+(value/mMaxy)*halfpixels;
+		}
+		else if(value<0){
+			//p = halfpixels-(value/mMiny)*.9*halfpixels;
+			p = halfpixels-(value/mMiny)*halfpixels;
+		}
+		else {
+			p = halfpixels;
+		}
+		return (int)p;
+	}
+	
+	/**
+	 * Convent x array of values to array of pixels
+	 */
+	private int[] toPixel(float pixels, ArrayList<Float> values) {
+	int[] pointsInt=new int[values.size()];
+	int i=0;
+	double p;
+	double startpixel=0.1*pixels;
+	
+	if (values.size()>1){
+		float min = values.get(0);
+		
+		for(Float f: values){
+			p = startpixel+((f.floatValue() - min)/mxInterval)*.9*pixels;
+			pointsInt[i] = (int)p;
+			i++;
+		}
+		}
+	return (pointsInt);
+}
+	
+	
+	/*private int[] toPixel(float pixels, float min, float max, ArrayList<Float> values) {
+		
+		int[] pointsInt=new int[values.size()];
+		
+		int i=0;
+		double p;
+		float halfpixels=pixels/2;
+		for(Float f: values){
+			
+			if(f.floatValue()>0){
+				p = halfpixels+((f.floatValue())/(max))*.9*halfpixels;
+			}
+			else if(f.floatValue()<0){
+				p = halfpixels-((f.floatValue())/(min))*.9*halfpixels;
+			}
+			else {
+				p = halfpixels;
+			}
+			
+			pointsInt[i] = (int)p;
+			i++;
+		}
+		
+		return (pointsInt);
+	}*/
+	
+}
+
