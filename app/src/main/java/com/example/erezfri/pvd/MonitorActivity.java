82c24c98916b994d57a0ec6e70abc1e494ffc03c
erez fridman <erezfri@gmail.com>
2015-08-12 23:07:48 +0300

    fix for the camera view

--- app/src/main/java/com/example/erezfri/pvd/MonitorActivity.java
+++ app/src/main/java/com/example/erezfri/pvd/MonitorActivity.java
@@ -8,8 +8,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
@@ -21,6 +24,8 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.TextureView;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.Toast;
@@ -31,7 +36,7 @@ import java.util.ArrayList;
 import java.util.Set;
 
 
-public class MonitorActivity extends ActionBarActivity implements TextureView.SurfaceTextureListener {
+public class MonitorActivity extends ActionBarActivity{
 
     private static final String UUID_SERIAL_PORT_PROFILE
             = "00001101-0000-1000-8000-00805F9B34FB";
@@ -61,6 +66,13 @@ public class MonitorActivity extends ActionBarActivity implements TextureView.Su
     private PlotDynamic mGraph;
     private ArrayList<byte[]> Packets;
 
+    //for the camera
+    private Context myContext;
+    private boolean cameraFront = false;
+    private CameraPreview mPreview;
+    private LinearLayout cameraPreview;
+    private Button capture;
+    private MediaRecorder mediaRecorder;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -78,10 +90,33 @@ public class MonitorActivity extends ActionBarActivity implements TextureView.Su
         }
 
         //start for camera preview
-        mTextureView = (TextureView)findViewById(R.id.textureView) ;
-        mTextureView.setSurfaceTextureListener(this);
+       // mTextureView = (TextureView)findViewById(R.id.textureView) ;
+       // mTextureView.setSurfaceTextureListener(this);
         //setContentView(mTextureView);
         //end for camera preview
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        myContext = this;
+        initialize();
+
+
+    }
+    private int findBackFacingCamera() {
+        int cameraId = -1;
+        // Search for the back facing camera
+        // get the number of cameras
+        int numberOfCameras = Camera.getNumberOfCameras();
+        // for every camera check
+        for (int i = 0; i < numberOfCameras; i++) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            Camera.getCameraInfo(i, info);
+            if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                cameraId = i;
+                cameraFront = false;
+                break;
+            }
+        }
+        return cameraId;
     }
 
       public void onSearchButtonClick(View view) {
@@ -120,38 +155,161 @@ public class MonitorActivity extends ActionBarActivity implements TextureView.Su
         return;
     }
     //start for camera preview
-    @Override
-    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-        mCamera = Camera.open();
+//    @Override
+//    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+//        mCamera = Camera.open();
+//
+//        Camera.Size previewSize = mCamera.getParameters().getPreviewSize();
+//        mTextureView.setLayoutParams(new FrameLayout.LayoutParams(
+//                previewSize.width / 2, previewSize.height / 2, Gravity.BOTTOM));
+//
+//        try {
+//            mCamera.setPreviewTexture(surface);
+//        } catch (IOException t) {
+//        }
+//
+//        mCamera.startPreview();
+//        mCamera.setDisplayOrientation(90);
+//
+//    }
+//    @Override
+//    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+//        // Ignored, the Camera does all the work for us
+//    }
+//    @Override
+//    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+//        mCamera.stopPreview();
+//        mCamera.release();
+//        return true;
+//    }
+//    @Override
+//    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+//        // Update your view here!
+//    }
+//    //end for camera preview
 
-        Camera.Size previewSize = mCamera.getParameters().getPreviewSize();
-        mTextureView.setLayoutParams(new FrameLayout.LayoutParams(
-                previewSize.width / 2, previewSize.height / 2, Gravity.BOTTOM));
+
+    public void onResume() {
+        super.onResume();
+        if (!hasCamera(myContext)) {
+            Toast toast = Toast.makeText(myContext, "Sorry, your phone does not have a camera!", Toast.LENGTH_LONG);
+            toast.show();
+            finish();
+        }
+        if (mCamera == null) {
+            // if the front facing camera does not exist
+//            if (findFrontFacingCamera() < 0) {
+//                Toast.makeText(this, "No front facing camera found.", Toast.LENGTH_LONG).show();
+//               // switchCamera.setVisibility(View.GONE);
+//            }
+            mCamera = Camera.open(findBackFacingCamera());
+            mCamera.setDisplayOrientation(90);
+            mPreview.refreshCamera(mCamera);
+        }
+    }
+    public void initialize() {
+        cameraPreview = (LinearLayout) findViewById(R.id.camera_preview);
+
+        mPreview = new CameraPreview(myContext, mCamera);
+        cameraPreview.addView(mPreview);
+
+
+        capture = (Button) findViewById(R.id.button_capture);
+        capture.setOnClickListener(captrureListener);
+
+       // switchCamera = (Button) findViewById(R.id.button_ChangeCamera);
+       // switchCamera.setOnClickListener(switchCameraListener);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // when on Pause, release camera in order to be used from other
+        // applications
+        releaseCamera();
+    }
+    private boolean hasCamera(Context context) {
+        // check if the device has camera
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    boolean recording = false;
+    View.OnClickListener captrureListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (recording) {
+                // stop recording and release camera
+                mediaRecorder.stop(); // stop the recording
+                releaseMediaRecorder(); // release the MediaRecorder object
+                Toast.makeText(MonitorActivity.this, "Video captured!", Toast.LENGTH_LONG).show();
+                recording = false;
+            } else {
+                if (!prepareMediaRecorder()) {
+                    Toast.makeText(MonitorActivity.this, "Fail in prepareMediaRecorder()!\n - Ended -", Toast.LENGTH_LONG).show();
+                    finish();
+                }
+                // work on UiThread for better performance
+                runOnUiThread(new Runnable() {
+                    public void run() {
+                        // If there are stories, add them to the table
+
+                        try {
+                            mediaRecorder.start();
+                        } catch (final Exception ex) {
+                            // Log.i("---","Exception in thread");
+                        }
+                    }
+                });
+
+                recording = true;
+            }
+        }
+    };
+    private void releaseMediaRecorder() {
+        if (mediaRecorder != null) {
+            mediaRecorder.reset(); // clear recorder configuration
+            mediaRecorder.release(); // release the recorder object
+            mediaRecorder = null;
+            mCamera.lock(); // lock camera for later use
+        }
+    }
+    private boolean prepareMediaRecorder() {
+
+        mediaRecorder = new MediaRecorder();
+
+        mCamera.unlock();
+        mediaRecorder.setCamera(mCamera);
+
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.CAMCORDER);
+        mediaRecorder.setVideoSource(MediaRecorder.VideoSource.CAMERA);
+
+        mediaRecorder.setProfile(CamcorderProfile.get(CamcorderProfile.QUALITY_720P));
+
+        mediaRecorder.setOutputFile("/sdcard/myvideo.mp4");
+        mediaRecorder.setMaxDuration(600000); // Set max duration 60 sec.
+        mediaRecorder.setMaxFileSize(50000000); // Set max file size 50M
 
         try {
-            mCamera.setPreviewTexture(surface);
-        } catch (IOException t) {
+            mediaRecorder.prepare();
+        } catch (IllegalStateException e) {
+            releaseMediaRecorder();
+            return false;
+        } catch (IOException e) {
+            releaseMediaRecorder();
+            return false;
         }
-
-        mCamera.startPreview();
-        mCamera.setDisplayOrientation(90);
-
-    }
-    @Override
-    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-        // Ignored, the Camera does all the work for us
-    }
-    @Override
-    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-        mCamera.stopPreview();
-        mCamera.release();
         return true;
+
     }
-    @Override
-    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-        // Update your view here!
+    private void releaseCamera() {
+        // stop and release camera
+        if (mCamera != null) {
+            mCamera.release();
+            mCamera = null;
+        }
     }
-    //end for camera preview
 
 
     //bluetooth
@@ -228,6 +386,11 @@ public class MonitorActivity extends ActionBarActivity implements TextureView.Su
         }
     }
 
+    public void videoFunc(){
+        Intent intent = new Intent(this, AndroidVideoCaptureExample.class);
+        startActivity(intent);
+    }
+
 
     // The Handler that gets information back from the BluetoothService
     private final Handler mHandler = new Handler() {
@@ -255,8 +418,7 @@ public class MonitorActivity extends ActionBarActivity implements TextureView.Su
                     String msgString = new String(readBuf);
                     if (msgString.startsWith("TAKEPICTURE"))
                     {
-                        //Plot();
-
+                        videoFunc();
                     }
                     Toast.makeText(getApplicationContext(), msgString, Toast.LENGTH_SHORT).show();
                     break;
